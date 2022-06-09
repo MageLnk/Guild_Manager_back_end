b4e37fa5d637966893d02ca83c3ba23fb3a33968
MageLink <mage.link.zhezard@gmail.com>
2022-06-09 07:39:28 -0400

    Starting ahora si

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Ola k ase");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Recibido");
+});
+
+app.listen(5000, () => {
+  console.log("Server started OK");
+});
