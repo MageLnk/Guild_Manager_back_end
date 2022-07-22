c2e3de2b839f0af660889c3c0d01db9fcaa73742
MageLink <mage.link.zhezard@gmail.com>
2022-07-22 18:05:15 -0400

    Externalizar la dummyData

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,64 +1,27 @@
 // Operators
 //import { v4 as uuidv4 } from "uuid";
 // Variables
-let dummyUsers = [
-  {
-    userName: "MageLink",
-    password: "1234567",
-    status: "admin",
-    games: [
-      { name: "Aion", active: false },
-      { name: "Black Desert", active: true },
-      { name: "Final Fantasy XIV", active: false },
-      { name: "Lost Ark", active: true },
-      { name: "World of Warcraft", active: true },
-    ],
-  },
-  {
-    userName: "MageLink",
-    password: "1234",
-    status: "admin",
-    games: [
-      { name: "Aion", active: false },
-      { name: "Black Desert", active: true },
-      { name: "Final Fantasy XIV", active: false },
-      { name: "Lost Ark", active: true },
-      { name: "World of Warcraft", active: true },
-    ],
-  },
-  {
-    userName: "Noxtror",
-    password: "123456",
-    status: "user",
-    games: [
-      { name: "Aion", active: true },
-      { name: "Black Desert", active: true },
-      { name: "Final Fantasy XIV", active: false },
-      { name: "Lost Ark", active: true },
-      { name: "World of Warcraft", active: false },
-    ],
-  },
-];
+import { dummyUsers } from "../../dummyData/dummyData.js";
 // App
 export const getAllGeneralUsers = (req, res) => {
   res.send(dummyUsers);
 };
 export const getUser = (req, res) => {
-  console.log(req.body);
   const checkUser = dummyUsers.find((check) => {
-    return check.userName === req.body.userName && check.password === req.body.password;
+    return (
+      check.secretData.userName === req.body.userName &&
+      check.secretData.password === req.body.password
+    );
   });
-
   if (checkUser === undefined) {
     res
       .status(406)
       .send({ msg: "Hay un problema con el usuario y contraseña", loginStatus: false });
     return;
   }
-  res
-    .status(200)
-    .send({
-      msg: "Todo ok, en un futuro enviaré el usuario que haga match con el logIn",
-      loginStatus: true,
-    });
+  res.status(200).send({
+    msg: "Todo ok, en un futuro enviaré el usuario que haga match con el logIn",
+    loginStatus: true,
+    gamesInfo: checkUser.publicData,
+  });
 };
---
+++ src/dummyData/dummyData.js
@@ -0,0 +1,74 @@
+export const dummyUsers = [
+  {
+    secretData: {
+      userName: "MageLink",
+      password: "1234567",
+      admin: false,
+      superAdmin: false,
+    },
+    publicData: {
+      games: [
+        { name: "Aion", active: true },
+        { name: "Black Desert", active: true },
+        { name: "Final Fantasy XIV", active: true },
+        { name: "Lineage II", active: true },
+        { name: "Lost Ark", active: true },
+        { name: "World of Warcraft", active: false },
+      ],
+    },
+  },
+  {
+    secretData: {
+      userName: "MageLink",
+      password: "1234",
+      admin: false,
+      superAdmin: false,
+    },
+    publicData: {
+      games: [
+        { name: "Aion", active: false },
+        { name: "Black Desert", active: true },
+        { name: "Final Fantasy XIV", active: false },
+        { name: "Lineage II", active: true },
+        { name: "Lost Ark", active: true },
+        { name: "World of Warcraft", active: true },
+      ],
+    },
+  },
+  {
+    secretData: {
+      userName: "Noxtror",
+      password: "12345",
+      admin: false,
+      superAdmin: false,
+    },
+    publicData: {
+      games: [
+        { name: "Aion", active: false },
+        { name: "Black Desert", active: true },
+        { name: "Final Fantasy XIV", active: false },
+        { name: "Lineage II", active: false },
+        { name: "Lost Ark", active: false },
+        { name: "World of Warcraft", active: false },
+      ],
+    },
+  },
+  {
+    secretData: {
+      userName: "JuanitoBower",
+      password: "123456789",
+      admin: false,
+      superAdmin: false,
+    },
+    publicData: {
+      games: [
+        { name: "Aion", active: true },
+        { name: "Black Desert", active: true },
+        { name: "Final Fantasy XIV", active: false },
+        { name: "Lineage II", active: true },
+        { name: "Lost Ark", active: true },
+        { name: "World of Warcraft", active: false },
+      ],
+    },
+  },
+];
